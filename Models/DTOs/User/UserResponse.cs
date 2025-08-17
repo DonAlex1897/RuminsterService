43340772b6cfa592041a122a43a4407ecef116bd
donalex1897 <kaviyani.ma@gmail.com>
2025-08-17 12:42:04 +0200

    fix: Correct indentation in UserResponse class properties

--- Models/DTOs/User/UserResponse.cs
+++ Models/DTOs/User/UserResponse.cs
@@ -10,7 +10,7 @@ namespace RuminsterBackend.Models.DTOs.User
 
         public string Email { get; set; }
 
-    public string? Name { get; set; }
+        public string? Name { get; set; }
 
         public List<string>? Roles { get; set; }
     }
