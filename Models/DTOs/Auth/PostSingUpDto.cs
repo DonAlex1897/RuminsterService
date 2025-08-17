fe5e4fcbfce31f824ffd4dfc5abf11c95598bb84
donalex1897 <kaviyani.ma@gmail.com>
2025-08-17 12:43:34 +0200

    fix: Correct indentation for Name property in PostSignUpDto class

--- Models/DTOs/Auth/PostSingUpDto.cs
+++ Models/DTOs/Auth/PostSingUpDto.cs
@@ -13,7 +13,7 @@ namespace RuminsterBackend.Models.DTOs.Auth
         public string Username { get; set; } = string.Empty;
         [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
-    public string? Name { get; set; }
+        public string? Name { get; set; }
         [Required, MinLength(8)]
         public string Password { get; set; } = string.Empty;
         [Required]
